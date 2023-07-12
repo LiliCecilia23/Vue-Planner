c793b1fef6cf0e4802acc5fd69321f9a7249588a
Lili Clift <liliclift@gmail.com>
2023-07-11 19:40:01 -0500

    auth files

--- vue-planner/src/main.js
+++ vue-planner/src/main.js
@@ -3,7 +3,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import GoogleAuth from '@/config/google_oAuth.js'
+const gauthOption = {
+  clientId: '1031252778292-qcnv14idkoqoefn54n6cltpjagjsvu02.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GoogleAuth, gauthOption);
+Vue.config.productionTip = false;
 const app = createApp(App)
 
 app.use(createPinia())
